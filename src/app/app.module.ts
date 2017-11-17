009901c2fe8f42b76681d6fa752262d91f26a921
Stephen Fluin <stephen.fluin@gmail.com>
2017-11-16 21:49:10 -0800

    Undo preload strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PreloadingStrategy, Route } from '@angular/router';
-
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-
-export class AppCustomPreloader implements PreloadingStrategy {
-  preload(route: Route, load: Function): Observable<any> {
-    console.log("preload on a route");
-    return route.data && route.data.preload ? load() : of(null);
-  }
-}
-
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -23,14 +11,9 @@ export class AppCustomPreloader implements PreloadingStrategy {
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'about', loadChildren: './about/about.module#AboutModule', data: { preload: false } },
-    ], {preloadingStrategy: AppCustomPreloader}),
+      { path: 'about', loadChildren: './about/about.module#AboutModule' },
+    ]),
   ],
-  providers: [AppCustomPreloader],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-
-
-
