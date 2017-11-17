c2fe6fa643dcd2f32a5ebeb17cd0a185eba89d88
Stephen Fluin <stephen.fluin@gmail.com>
2017-11-16 22:08:02 -0800

    Add search to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'about', loadChildren: './about/about.module#AboutModule' },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  repos;
+  constructor(http: HttpClient) {
+    const path = 'https://api.github.com/search/repositories?q=angular';
+    this.repos = http.get<any>(path).pipe(
+      map(results => results.items)
+    );
   }
 
+  ngOnInit() {}
 }
