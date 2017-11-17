e5b7f314817b54ac0c4746a6c30485d736b8da7d
Stephen Fluin <stephen.fluin@gmail.com>
2017-11-16 21:36:57 -0800

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PreloadingStrategy, Route } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+export class AppCustomPreloader implements PreloadingStrategy {
+  preload(route: Route, load: Function): Observable<any> {
+    console.log("preload on a route");
+    return route.data && route.data.preload ? load() : of(null);
+  }
+}
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', loadChildren: './about/about.module#AboutModule', data: { preload: false } },
+    ], {preloadingStrategy: AppCustomPreloader}),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AppCustomPreloader],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+
+
+
